5402b26117642bf0ed26b14b7e9dc516a894bbdc
FerrnA <arriondovfernando@gmail.com>
2022-07-21 22:56:46 -0300

    Change endpoint method and remove unused import in users.router.js

--- routes/users/users.router.js
+++ routes/users/users.router.js
@@ -1,10 +1,9 @@
 const { Router } = require("express");
 const { getAllUsers } = require("./controllers/getAll.controller");
 const isAdmin = require("../common/isAdmin");
-const { getOneUser } = require("../../controllers/usersControllers");
 
 const users = Router();
 
-users.route("/all").post([isAdmin, getAllUsers]);
+users.route("/all").get([isAdmin, getAllUsers]);
 
 module.exports = users;
