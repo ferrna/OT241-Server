ccbac2c7d39698bac2b5f4476cbeda824ae906c5
TheJB4 <juanmer382@gmail.com>
2022-07-31 11:21:43 -0300

    categories

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,8 @@
 var express = require("express");
-const mails = require("./mails/mails.router");
 const news = require("./news/news.router");
 const users = require("./users/users.router");
 var router = express.Router();
 const mails = require("./mails/mails.router");
-const news = require("./news/news.router");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
