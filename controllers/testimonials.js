2ea9965f24873ce04890293ac73b7802aa7e72ef
NicoMunoz909 <nicolas.munoz@outlook.es>
2022-08-01 19:16:29 -0300

    Add validations in post controller

--- controllers/testimonials.js
+++ controllers/testimonials.js
@@ -10,8 +10,28 @@ const createTestimonial = async (req,res) => {
     })
     return res.json(newTestimonial)
   } catch(err){
-    console.log(err)
-    return err
+    if (err.errors[0].validatorKey === "notEmpty") {
+      switch (err.errors[0].path) {
+        case 'name':
+          return res.status(400).json("Name cannot be empty");
+        case 'content':
+          return res.status(400).json("Content cannot be empty");
+        default:
+          break;
+      }
+    } else if (err.errors[0].validatorKey === "is_null") {
+      switch (err.errors[0].path) {
+        case 'name':
+          return res.status(400).json("Name cannot be null");
+        case 'content':
+          return res.status(400).json("Content cannot be null");
+        default:
+          break;
+      }
+    } else {
+      return res.status(400).json(err)
+      console.log(err)
+    }
   }
 }
 
