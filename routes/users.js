c72518d3ba79edd15d06d8c06b6724122482ea0d
Fernando Arriondo <arriondovfernando@gmail.com>
2022-08-03 03:53:25 -0300

    Login user automatically after registering

--- routes/users.js
+++ routes/users.js
@@ -1,84 +1,46 @@
-var express = require('express');
-const {getAllUsers,createUser,getOneUser, deleteUser} = require('../controllers/usersControllers');
+var express = require("express");
+const { createUser, getOneUser } = require("../controllers/usersControllers");
 var router = express.Router();
-let verification = express.Router()
-let jwt = require('jsonwebtoken')
+let verification = express.Router();
+let jwt = require("jsonwebtoken");
 
-let {body,validationResult} = require('express-validator')
+let { body, validationResult } = require("express-validator");
 
+verification.use((req, res, next) => {
+  let token = req.headers["x-access-token"] || req.headers["authorization"];
+  if (!token) {
+    res.status(404).send({ error: "es necesario el token para la authenticacion" });
+  }
+  if (token.startsWith("Bearer ")) {
+    token = token.slice(7, token.length);
+  }
+  if (token) {
+    jwt.verify(token, "juan123", (error, decoded) => {
+      if (error) return res.json({ message: "token no es valido" });
+      else {
+        req.decoded = decoded;
+        next();
+      }
+    });
+  }
+});
 
-verification.use((req,res,next) => {
-    let token = req.headers['x-access-token'] || req.headers['authorization']
-    if(!token){
-        res.status(404).send({error: 'es necesario el token para la authenticacion'})
-    }
-    if(token.startsWith('Bearer ')){
-        token = token.slice(7, token.length)
-    }
-    if(token){
-        jwt.verify(token,'juan123',(error,decoded)=>{
-            if(error) return res.json({message:'token no es valido'})
-            else{
-                req.decoded = decoded
-                next()
-            }
-        })
-    }
-})
+router.post("/register", [
+  body("firstName", "Ingrese un nombre").exists().notEmpty().isLength({ max: 15 }),
+  body("lastName", "Ingrese un apellido").exists().notEmpty().isLength({ max: 15 }),
+  body("email", "Ingrese un email valido").exists().notEmpty().isEmail(),
+  body("password", "Ingrese un password valido").exists().notEmpty().isLength({ min: 8 }),
+  createUser,
+]);
 
-/* GET users listing. */
-router.get('/', getAllUsers);
+router.post("/login", [
+  body("email", "Ingrese un email valido").exists().notEmpty().isEmail(),
+  body("password", "Ingrese un password valido").exists().notEmpty(),
+  getOneUser,
+]);
 
-router.post('/auth/register',[
-    body('firstName','Ingrese un nombre')
-        .exists()
-        .notEmpty()
-        .isLength({max:15}),
-    body('lastName','Ingrese un apellido')
-        .exists()
-        .notEmpty()
-        .isLength({max:15}),
-    body('email','Ingrese un email valido')
-        .exists()
-        .notEmpty()
-        .isEmail(),
-    body('password','Ingrese un password valido') 
-        .exists()
-        .notEmpty()
-        .isLength({min:8})
-],createUser)
-
-router.post('/auth/login',[
-    body('email','Ingrese un email valido')
-    .exists()
-    .notEmpty()
-    .isEmail(),
-body('password','Ingrese un password valido') 
-    .exists()
-    .notEmpty()
-],getOneUser)
-
-
-router.get('/auth/login',verification,(req,res)=>{
-    res.json({ok: true})
-})
-
-router.get('/organizations',verification,(req,res)=>{
-    res.send('hola')
-})
-
-router
-    .route('/:id')
-    .delete(async(req, res) => {
-        try {
-            const {id} = req.params
-            await deleteUser(id)
-            res.send({message: `Usuario con id: ${id} ha sido exitosamente borrado`})
-            
-        } catch (error) {
-            console.log(error)
-        }
-        
-    })
+router.get("/login", verification, (req, res) => {
+  res.json({ ok: true });
+});
 
 module.exports = router;
--- routes/users/users.router.js
+++ routes/users/users.router.js
@@ -1,9 +1,19 @@
 const { Router } = require("express");
-const { getAllUsers } = require("./controllers/getAll.controller");
+const { getAllUsers, deleteUser } = require("../../controllers/usersControllers");
 const isAdmin = require("../common/isAdmin");
 
 const users = Router();
 
 users.route("/all").get([isAdmin, getAllUsers]);
+
+users.route("/:id").delete(async (req, res) => {
+  try {
+    const { id } = req.params;
+    await deleteUser(id);
+    res.send({ message: `Usuario con id: ${id} ha sido exitosamente borrado` });
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 module.exports = users;
