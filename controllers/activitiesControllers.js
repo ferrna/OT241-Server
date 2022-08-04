5ab06d2c53ee788046d88f077e46f57a160eff74
NicoMunoz909 <nicolas.munoz@outlook.es>
2022-08-03 23:59:53 -0300

    Create update controller

--- controllers/activitiesControllers.js
+++ controllers/activitiesControllers.js
@@ -1,4 +1,4 @@
-let { Activitiy } = require("../models");
+const { activities } = require("../models");
 let { body, validationResult } = require("express-validator");
 
 const createActivity = async (req, res, next) => {
@@ -21,6 +21,26 @@ const createActivity = async (req, res, next) => {
   }
 };
 
+const updateActivity =  (req, res, next) => {
+  activities.update({ ...req.body }, {
+    where: {
+      id: req.params.id
+    }
+  })
+  .then((update) => {
+    if (update[0] === 0) {
+      throw err
+      //return res.status(400).json('There is not an activity with that ID')
+    } else {
+      return res.json(activities.findByPk(req.params.id))
+    }
+  })
+  .catch(() => {
+    return res.status(400).json('There is not an activity with that ID')
+  })
+};
+
 module.exports = {
   createActivity,
+  updateActivity
 };
--- routes/activities/activities.router.js
+++ routes/activities/activities.router.js
@@ -1,9 +1,9 @@
 const { Router } = require("express");
-const { postActivity } = require("./controllers/post.controller");
+const { postActivity, updateActivity } = require("./controllers/post.controller");
 
 const activitiesRouter = Router();
 
 activitiesRouter.route("/").post(postActivity);
-activitiesRouter.put('/:id')
+activitiesRouter.put('/:id', updateActivity);
 
 module.exports = activitiesRouter;
