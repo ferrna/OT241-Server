ab02c0509731a88335335c68b88fb788c8535625
NicoMunoz909 <nicolas.munoz@outlook.es>
2022-08-07 16:50:47 -0300

    Write get controller

--- controllers/categories.js
+++ controllers/categories.js
@@ -1,6 +1,16 @@
 const { Categories } = require('../models')
 
-const getCategories = () => {}
+const getCategories = (req, res) => {
+  const categories = Categories.FindAll({
+    attributes: ['name']
+  })
+  .then(res => {
+    return res.json(categories)
+  })
+  .catch(err => {
+    return err
+  })
+}
 
 module.exports = {
   getCategories
