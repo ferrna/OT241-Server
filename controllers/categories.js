5518b76974ce8bfb2381b49e907b7fe00530fe81
NicoMunoz909 <nicolas.munoz@outlook.es>
2022-08-07 17:49:49 -0300

    Write update controller

--- controllers/categories.js
+++ controllers/categories.js
@@ -46,7 +46,48 @@ const createCategory = (req, res) => {
   })
 }
 
-const updateCategory = () => {}
+const updateCategory = (req, res) => {
+  categories.update({...req.body})
+  .then((update) => {
+    if (update[0] === 0) {
+      const error = "There is not an activity with that ID";
+      throw error;
+    } else {
+      const category = categories.findByPk(req.params.id);
+      return category;
+    }
+  })
+  .then( category => {
+    return res.json(category)
+  })
+  .catch( err => {
+    if (err.errors[0].validatorKey === "notEmpty") {
+      switch (err.errors[0].path) {
+        case 'name':
+          return res.status(400).json("Name cannot be empty");
+        default:
+          break;
+      }
+    } else if (err.errors[0].validatorKey === "is_null") {
+      switch (err.errors[0].path) {
+        case 'name':
+          return res.status(400).json("Name cannot be null");
+        default:
+          break;
+      }
+    } else if (err.errors[0].validatorKey === "is") {
+      switch (err.errors[0].path) {
+        case 'name':
+          return res.status(400).json("Name format is incorrect");
+        default:
+          break;
+      }
+    }
+    else {
+      return res.status(400).json(err)
+    }
+  })
+}
 
 module.exports = {
   getCategories,
