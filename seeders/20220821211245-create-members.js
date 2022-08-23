914e654d6b4ee5fea1cc334762855b0c63d71dce
ignacio <ignacio.moenen@gmail.com>
2022-08-22 23:12:36 -0400

    adding images to members

--- seeders/20220821211245-create-members.js
+++ seeders/20220821211245-create-members.js
@@ -5,7 +5,7 @@ module.exports = {
     await queryInterface.bulkInsert('members', [
       {
       name: 'María Iraola',
-      image: '4038752f7d296aec756e9938b93681b0',
+      image: '975fa1dd346dbe8220484b1d6ae64838',
       role: 'Presidenta',
       description: "María estudió economía y se especializó en economía para el desarrollo. Comenzó como voluntaria en la fundación y fue quien promovió el crecimiento y la organización de la institución acompañando la transformación de un simple comedor barrial al centro comunitario de atención integral que es hoy en día",
       createdAt: new Date(),
@@ -13,7 +13,7 @@ module.exports = {
      },
      {
       name: 'Marita Gomez',
-      image: '2bfa925f7022841ca1b4c3e54431631a',
+      image: '2ba2da7cead0ef2d6df080e094697280',
       role: 'Fundadora',
       description: "Marita estudió la carrera de nutrición y se especializó en nutrición infantil. Toda la vida fue voluntaria en distintos espacios en el barrio hasta que decidió abrir un comedor propio. Comenzó trabajando con 5 familias y culminó su trabajo transformando Somos Más en la organización que es hoy.",
       createdAt: new Date(),
@@ -21,7 +21,7 @@ module.exports = {
      },
      {
       name: 'Miriam Rodriguez',
-      image: '8074bdef3ec60a66cff069478130e14e',
+      image: '989724b0620859bff568b36cee98cadb',
       role: 'Terapista Ocupacional',
       description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent elementum at nulla id lacinia. Sed nec ex accumsan, viverra tellus nec, fermentum erat. Sed tincidunt risus molestie sem faucibus, sit amet fermentum.",
       createdAt: new Date(),
@@ -29,7 +29,7 @@ module.exports = {
      },
      {
       name: 'Cecilia Mendez',
-      image: '35fa1abb050b646c843ef1a26150ee9e',
+      image: '00a7e0a2392d43304ccdec3487ca35f9',
       role: 'Psicopedagoga',
       description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent elementum at nulla id lacinia. Sed nec ex accumsan, viverra tellus nec, fermentum erat. Sed tincidunt risus molestie sem faucibus, sit amet fermentum.",
       createdAt: new Date(),
@@ -37,7 +37,7 @@ module.exports = {
      },
      {
       name: 'Mario Fuentes',
-      image: 'a7be39f693f676ce8650dfc1776ed33f',
+      image: 'ccc1086028f85b8d43180401e7322498',
       role: 'Psicólogo',
       description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent elementum at nulla id lacinia. Sed nec ex accumsan, viverra tellus nec, fermentum erat. Sed tincidunt risus molestie sem faucibus, sit amet fermentum.",
       createdAt: new Date(),
@@ -45,7 +45,7 @@ module.exports = {
      },
      {
       name: 'Rodrigo Fuente',
-      image: '25b08fbf03657faa9b34f257a6bb49ab',
+      image: '9f79631597f42dc5cf194f35dccfc938',
       role: 'Contador',
       description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent elementum at nulla id lacinia. Sed nec ex accumsan, viverra tellus nec, fermentum erat. Sed tincidunt risus molestie sem faucibus, sit amet fermentum.",
       createdAt: new Date(),
@@ -53,7 +53,7 @@ module.exports = {
      },
      {
       name: 'Maria Garcia',
-      image: '658d00149fd78dfd0f2a417c5776349c',
+      image: '901268ec8bc43a0a847a0d9da93e0285',
       role: 'Profesora de Artes Dramáticas',
       description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent elementum at nulla id lacinia. Sed nec ex accumsan, viverra tellus nec, fermentum erat. Sed tincidunt risus molestie sem faucibus, sit amet fermentum.",
       createdAt: new Date(),
@@ -61,7 +61,7 @@ module.exports = {
      },
      {
       name: 'Marco Fernandez',
-      image: '6b2d64adf500be981437a115452be154',
+      image: '3571914eb4669d861d040f8168d69de7',
       role: 'Profesor de Educación Física',
       description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent elementum at nulla id lacinia. Sed nec ex accumsan, viverra tellus nec, fermentum erat. Sed tincidunt risus molestie sem faucibus, sit amet fermentum.",
       createdAt: new Date(),
