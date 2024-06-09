0f9ec0a9b0bf60c81ae9326268ac05b799ca7c17
Fernando Arriondo <arriondovfernando@gmail.com>
2024-06-09 18:58:16 -0300

    change aws envs names, postgres dialect on prod

--- helpers/S3AWService.js
+++ helpers/S3AWService.js
@@ -4,10 +4,10 @@ const S3 = require("aws-sdk/clients/s3")
 
 
 //accesos desde el ENV
-const bucketName = process.env.AWS_BUCKET_NAME
-const region = process.env.AWS_BUCKET_REGION
-const accessKeyId = process.env.AWS_ACCESS_KEY
-const secretAccessKey = process.env.AWS_SECRET_KEY
+const bucketName = process.env.SM_AWS_BUCKET_NAME
+const region = process.env.SM_AWS_BUCKET_REGION
+const accessKeyId = process.env.SM_AWS_ACCESS_KEY
+const secretAccessKey = process.env.SM_AWS_SECRET_KEY
 
 const s3 = new S3({
     region,
